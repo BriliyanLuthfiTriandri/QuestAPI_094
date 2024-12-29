85a71333cdb984b8460338afb17a5aafbc082021
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-30 05:22:17 +0700

    Menambahkan fungsi toInsertUiEvent untuk mengonversi objek Mahasiswa ke InsertUiEvent

---
+++ app/src/main/java/com/example/pertemuan12database/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.pertemuan12database.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12database.model.Mahasiswa
+import com.example.pertemuan12database.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
