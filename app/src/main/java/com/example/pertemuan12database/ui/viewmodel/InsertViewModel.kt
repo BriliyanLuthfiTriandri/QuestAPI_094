aaa4a7e61fb7b0001d399f06f11c6c0764b12d57
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-30 05:26:14 +0700

    Menambahkan InsertViewModel untuk mengelola state input mahasiswa dan menyimpan data mahasiswa

--- app/src/main/java/com/example/pertemuan12database/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12database/ui/viewmodel/InsertViewModel.kt
@@ -10,7 +10,27 @@ import com.example.pertemuan12database.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
 
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(
+            insertUiEvent = insertUiEvent
+        )
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+}
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent(),
