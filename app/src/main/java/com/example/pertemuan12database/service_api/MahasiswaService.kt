6e78fdfe1c7b431864e097f9fa5b08aba5055ea7
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-16 15:22:01 +0700

    merubah iterface MahasiswaService

--- app/src/main/java/com/example/pertemuan12database/service_api/MahasiswaService.kt
+++ app/src/main/java/com/example/pertemuan12database/service_api/MahasiswaService.kt
@@ -1,6 +1,8 @@
 package com.example.pertemuan12database.service_api
 
 import com.example.pertemuan12database.model.Mahasiswa
+import com.example.pertemuan12database.model.MahasiswaResponse
+import com.example.pertemuan12database.model.MahasiswaResponseDetail
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -8,6 +10,7 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MahasiswaService {
@@ -16,18 +19,33 @@ interface MahasiswaService {
         "Content-Type: application/json",
     )
 
-    @GET("bacamahasiswa.php")
-    suspend fun getMahasiswa(): List<Mahasiswa>
+//    @GET("bacamahasiswa.php")
+//    suspend fun getMahasiswa(): List<Mahasiswa>
+//
+//    @GET("baca1mahasiswa.php/{nim}")
+//    suspend fun getMahasiswaByNim(@Query("nim") nim: String): Mahasiswa
+//
+//    @POST("insertmahasiswa.php")
+//    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
+//
+//    @PUT("editmahasiswa.php/{nim}")
+//    suspend fun updateMahasiswa(@Query("nim") nim: String, @Body mahasiswa: Mahasiswa)
+//
+//    @DELETE("deletemahasiswa.php/{nim}")
+//    suspend fun deleteMahasiswa(@Query("nim") nim: String): Response<Void>
 
-    @GET("baca1mahasiswa.php/{nim}")
-    suspend fun getMahasiswaByNim(@Query("nim") nim: String): Mahasiswa
+    @GET(".")
+    suspend fun getMahasiswa(): MahasiswaResponse
 
-    @POST("insertmahasiswa.php")
+    @GET("{nim}")
+    suspend fun getMahasiswaByNim(@Path("nim") nim: String): MahasiswaResponseDetail
+
+    @POST("store")
     suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
 
-    @PUT("editmahasiswa.php/{nim}")
-    suspend fun updateMahasiswa(@Query("nim") nim: String, @Body mahasiswa: Mahasiswa)
+    @PUT("{nim}")
+    suspend fun updateMahasiswa(@Path("nim") nim: String, @Body mahasiswa: Mahasiswa)
 
-    @DELETE("deletemahasiswa.php/{nim}")
-    suspend fun deleteMahasiswa(@Query("nim") nim: String): Response<Void>
+    @DELETE("{nim}")
+    suspend fun deleteMahasiswa(@Path("nim") nim: String): Response<Void>
 }
