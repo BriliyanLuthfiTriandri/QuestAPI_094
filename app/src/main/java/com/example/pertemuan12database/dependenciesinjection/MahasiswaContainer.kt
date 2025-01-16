dccb1419cc115c1e2f8fd2944cce0939518df3c3
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-16 15:18:05 +0700

    merubah localhost

--- app/src/main/java/com/example/pertemuan12database/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan12database/dependenciesinjection/MahasiswaContainer.kt
@@ -14,7 +14,7 @@ interface AppContainer {
 
 class MahasiswaContainer : AppContainer {
 
-    private val baseUrl = "http://10.0.2.2:80/umyTI/" // localhost diganti ip kalo di run di hp
+    private val baseUrl = "http://10.0.2.2:3000/api/mahasiswa/" // localhost diganti ip kalo di run di hp
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
